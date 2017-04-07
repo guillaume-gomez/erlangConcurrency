1db4a5215b61e337f8f09c714d7cbccd561efcb5
Gomez Guillaume <guillaume.gomez846@gmail.com>
2017-04-07 19:01:44 +0200

    finish live coding

--- week1.erl
+++ week1.erl
@@ -1,5 +1,5 @@
 -module(week1).
--export([]).
+-export([start/0, test/0, init/0]).
 
 allocate({[], Allocated}, _Pid) ->
   {
@@ -7,12 +7,45 @@ allocate({[], Allocated}, _Pid) ->
     {error, no_frequency}
   };
 
-allocate({[Freq| Free] , Allocated}, _Pid) ->
+allocate({[Freq| Free] , Allocated}, Pid) ->
   {
     {Free, [{Freq, Pid} | Allocated]},
     {ok, Freq}
-  };
+  }.
 
 deallocate({Free, Allocated}, Freq) ->
   NewAllocated = lists:keydelete(Freq, 1, Allocated),
   {[Freq, Free], NewAllocated}.
+
+loop(Frequencies) ->
+  receive
+    {request, Pid, allocate} ->
+      { NewFrequencies, Reply } = allocate(Frequencies, Pid),
+      Pid ! {reply, Reply},
+      loop(NewFrequencies);
+
+    {request, Pid, {deallocate, Freq}} ->
+      NewFrequencies = deallocate(Frequencies, Freq),
+      Pid ! { reply, ok },
+      loop(NewFrequencies);
+
+    {request, Pid, stop} ->
+      Pid ! { reply, stopped }
+  end.
+
+
+start() ->
+  ServerPid = spawn(week1, init, []).
+
+init() ->
+  Frequencies = { get_frequencies(), [] },
+  loop(Frequencies).
+
+
+% tool function
+  get_frequencies() -> [10,11,12,13,14,15,16].
+
+test() ->
+  Pid = start(),
+  Pid ! {request, self(), allocate},
+  receive {reply, Reply} -> Reply end.
